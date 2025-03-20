166be7617aa56b6c94b18568b0ea6af1d34c2c4e
Informatica <alhelimiishak@gmail.com>
2025-03-20 14:32:46 +0100

    servidor

--- lib/src/main/java/com/example/lib/Nada.java
+++
@@ -1,7 +0,0 @@
-package com.example.lib;
-
-public class Nada {
-    public static void main(String[] args) {
-        System.out.println("hoal");
-    }
-}
--- lib/src/main/java/com/example/lib/Server.java
+++ lib/src/main/java/com/example/lib/Server.java
@@ -9,23 +9,40 @@ import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
-        int port =12345;
+        int port = 12345;
         System.out.println("Encendido");
-        try (ServerSocket serverSocket=new ServerSocket(port)){
-            Socket clientSocket =serverSocket.accept();
-            System.out.println("Cliente conectado "+clientSocket.getInetAddress());
-            BufferedReader in =new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter out =new PrintWriter(clientSocket.getOutputStream(),true);
-            String mensaje=in.readLine();
-            if(mensaje.equals("Ishak")){
-                out.println("Acceso Exitosos");
-            }else{
-                out.println("Acceso denegado");
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Cliente conectado " + clientSocket.getInetAddress());
+
+                new Thread(() -> agregarCliente(clientSocket)).start();
             }
-            
-            clientSocket.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
+
+    private static void agregarCliente(Socket clientSocket) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+            String mensaje = in.readLine();
+            if ("Ishak".equals(mensaje)) {
+                out.println("Acceso Exitoso");
+            } else {
+                out.println("Acceso Denegado");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
